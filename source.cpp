f6ad61d49b3b324d30c817481cabc77035c8894c
Duonghtd <47000306+Duonghtd@users.noreply.github.com>
2019-03-02 21:25:59 +0700

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,57 @@
+#include <Windows.h>
+#include <iostream>
+#include "code.h"
+void khoidau(toadoran *toado) {
+	song = true;
+	system("cls");
+	setthucan(tdthucan,toado);
+	textcolor(2);
+	setran(toado);
+	vekhung();
+	veran(toado);
+	phai = true;
+	setthucan2(tdspc,toado);
+}
+int main() {
+	SetWindow(80, 30);
+	setFullScreen();
+	for (;;) {
+		Nocursortype();
+		giaodien();
+		chon();
+		xuatdiemcao();
+		controlspeed();
+		if (c1 == true || c2 == true) {
+			khoidau(toado);
+			while(song==true) {
+				vekhungdb();
+				benphai();
+				Nocursortype();
+				vekhung();
+				thucan(tdthucan,toado);
+				if ((int)ti % 10 == 0) {
+					thucandb(tdspc);
+				}
+				timdiemcao();
+				dichuyenran(toado);
+				switch (c1) {
+				case true: {
+					t1(toado);
+			
+					break;
+				}
+				case false: {
+					t2(toado);
+					break;
+				}
+				}
+				canduoi(toado);
+
+				veran(toado);
+				xulytocdo(toado);
+			}
+			system("cls");
+		}
+	}
+	return 0;
+}
