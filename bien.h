5f427deaa9c26c266772a808c1b75c34f363af2e
Duonghtd <47000306+Duonghtd@users.noreply.github.com>
2019-03-02 21:23:51 +0700

    Create bien.h

---
+++ bien.h
@@ -0,0 +1,37 @@
+using namespace std;
+struct toadothucan {
+	int X;
+	int Y;
+};
+int toadochuot = 8;
+int toadochuot2 = 8;
+struct toadothucan tdthucan[1];	
+int sodot = 3;
+bool phai = false;
+bool trai = false;
+bool tren = false;
+bool duoi = false;
+bool c1 = false;
+bool c2 = false;
+struct toadoran {
+	int x;
+	int y;
+};
+struct  toadoran toado[40] ;
+float ti = 0.0		;
+bool song;
+int diem = 0;
+int diemcao = 0;
+bool xuatdiemcao1 = false;
+bool tocdogameran = false;
+int sp = 0;
+int mauran = 12;
+bool spt;
+int tang = 50;
+int spg = 100;
+struct toadothucan2{
+	int X;
+	int Y;
+};
+struct toadothucan2 tdspc[1];
+bool tdspct;
